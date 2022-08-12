8481de304a5c1f6eb9dede65757a39c2bfc13720
Zsargul <kubakerlino@gmail.com>
2022-08-12 22:18:31 +0100

    modified:   config.h

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
@@ -6,6 +6,6 @@ static const Block blocks[] = {
 	{"", "date '+%A, %B %d [%H:%M]'",					5,		0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// Sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
